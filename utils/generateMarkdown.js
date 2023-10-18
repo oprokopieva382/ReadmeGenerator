805503dd6a15016b1d7015f40c4ebc5856b1eb30
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-10-17 23:22:37 -0500

    create renderLicenseBadge func but still want to check in action, and finish renderLicenseLink func

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,32 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  const LicenseBadgeColor = {
+    MIT: "MIT-yellow.svg",
+    GNU: "GPLv3-blue.svg",
+    Apache: "Apache%202.0-blue.svg",
+  };
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  if (LicenseBadgeColor) {
+    return `https://img.shields.io/badge/License-${LicenseBadgeColor[license]}`;
+  } else {
+    return "";
+  }
+}
+
+//function that returns the license link
+function renderLicenseLink(license) {
+  const LicenseLinks = {
+    MIT: "https://opensource.org/licenses/MIT",
+    GNU: "https://www.gnu.org/licenses/gpl-3.0",
+    Apache: "https://www.apache.org/licenses/LICENSE-2.0",
+  };
+
+  if(LicenseLinks) {
+    return LicenseLinks[license]
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,6 +34,9 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let badge = renderLicenseBadge(data.license);
+  let link = renderLicenseLink(data.license);
+
   return `
   # ${data.title}
 
@@ -33,8 +58,8 @@ function generateMarkdown(data) {
   ${data.usage}
 
   ## License
-  //! still need to update for badge
-  [!License Badge]()${renderLicenseLink(data.license)}
+  //! still need to check if work
+  [!License Badge](${badge})(${link})
 
   ## Contributing
   ${data.contributing}
@@ -43,9 +68,7 @@ function generateMarkdown(data) {
   ${data.tests}
 
   ## Questions
-  If you have any questions find me on [GitHub](https://github.com/${
-    data.username
-  }) or feel free email me ${data.email}
+  If you have any questions find me on [GitHub](https://github.com/${data.username}) or feel free email me ${data.email}
 
 
 `;

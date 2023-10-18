edfbe42ab55532fc8c3340875d3172b8586dba7b
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-10-17 22:22:46 -0500

    add source code

---
+++ index.js
@@ -0,0 +1,30 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Enter your project title:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter your project description:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter your project description:",
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();

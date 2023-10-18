dcd435bad0bb5af2fbad5c42ddb243ae9fda47c3
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-10-17 23:32:33 -0500

    update comments for all functions

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
+//array of questions for user input
 const questions = [
   {
     type: "input",
@@ -53,14 +53,14 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
+//function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => {
     err ? console.error(err) : console.log("README.md successfully generated!");
   });
 }
 
-// TODO: Create a function to initialize app
+//function to initialize app
 function init() {
   inquirer.prompt(questions).then((value) => {
     const generateMarkdown = generateMarkdown(value);
